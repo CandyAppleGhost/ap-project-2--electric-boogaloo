07934859c34bebe52a21193e5bd29e0671011398
CandyAppleGhost <150103917+CandyAppleGhost@users.noreply.github.com>
2024-02-09 11:29:19 -0800

    2/9

--- main.ts
+++ main.ts
@@ -1,7 +1,18 @@
 namespace SpriteKind {
     export const Fans = SpriteKind.create()
     export const chad = SpriteKind.create()
+    export const youIG = SpriteKind.create()
+    export const stageclue1 = SpriteKind.create()
+    export const fansclue1 = SpriteKind.create()
+    export const bodyclue1 = SpriteKind.create()
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.stageclue1, function (sprite, otherSprite) {
+	
+})
+let selector_baby: Sprite = null
+let body_clue_1: Sprite = null
+let fans_clue_1: Sprite = null
+let stage_clue_1: Sprite = null
 game.splash("Murder Mystery IG", "By: Jade")
 pause(100)
 game.showLongText("Are you ready for the best night of your lives?", DialogLayout.Bottom)
@@ -30,7 +41,7 @@ game.showLongText("Chad: What's up, everyone!", DialogLayout.Bottom)
 game.showLongText("Chad: Welcome to my show!", DialogLayout.Bottom)
 game.showLongText("Chad: Thank you all for coming out to support me!", DialogLayout.Bottom)
 game.showLongText("Chad: Let's get started!", DialogLayout.Bottom)
-pause(100)
+pause(200)
 sprites.destroy(chad)
 scene.setBackgroundImage(assets.image`black background`)
 scene.cameraShake(4, 100)
@@ -42,7 +53,7 @@ scene.setBackgroundImage(assets.image`black background`)
 scene.cameraShake(4, 100)
 pause(500)
 scene.setBackgroundImage(assets.image`Ded Chad Stage`)
-let ded_chad = sprites.create(assets.image`ded chad`, SpriteKind.Player)
+let ded_chad = sprites.create(assets.image`ded chad`, SpriteKind.chad)
 ded_chad.setPosition(75, 90)
 pause(500)
 game.setDialogCursor(img`
@@ -94,9 +105,16 @@ game.splash("Chad has died. It is up to", "you to find out who did it")
 sprites.destroy(ded_chad)
 scene.setBackgroundImage(assets.image`internal thoughts moment`)
 pause(500)
-story.showPlayerChoices("Look for clues?", "Ask around for more information?")
-if (story.checkLastAnswer("Look for clues?")) {
+story.showPlayerChoices("Look for clues", "")
+if (story.checkLastAnswer("Look for clues")) {
     scene.setBackgroundImage(assets.image`clue time baby`)
-} else {
-	
+    stage_clue_1 = sprites.create(assets.image`stage clue 1`, SpriteKind.stageclue1)
+    stage_clue_1.setPosition(26, 45)
+    fans_clue_1 = sprites.create(assets.image`fans clue 1`, SpriteKind.fansclue1)
+    fans_clue_1.setPosition(78, 36)
+    body_clue_1 = sprites.create(assets.image`body clue 1`, SpriteKind.bodyclue1)
+    body_clue_1.setPosition(128, 42)
+    selector_baby = sprites.create(assets.image`click click`, SpriteKind.Player)
+    selector_baby.setPosition(130, 90)
+    controller.moveSprite(selector_baby)
 }
